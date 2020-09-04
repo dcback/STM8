6915a362e35547832bc5405794ce5318f89de85d
DC Back <45322614+dcback@users.noreply.github.com>
2020-09-05 07:13:34 +0900

    Add files via upload

--- analogInOutSerial.ino
+++ analogInOutSerial.ino
@@ -1,5 +1,5 @@
 /*
-  https://github.com/dcback/STM8/blob/master/Hardware/09-stm8-image3801.png
+  https://github.com/dcback/STM8/blob/master/stm8_pinout.png
   https://github.com/tenbaht/sduino/blob/development/sduino/stm8/libraries/Generic_Examples/examples/03.Analog/AnalogInOutSerial/AnalogInOutSerial.ino
   [STM8 Pinout]
   PA1=0 PA2=1 PA3=2 PB5=3 PB4=4  PC3=5 PC4=6=A0 PC5=7 PC6=8 PC7=9 PD1=10 PD2=11=A1 PD3=12=A2 PD4=13 PD5=14=A3=TX PD6=15=A4=RX
